2a768e1f5d192df51f6ad0b3e271a0c9470294de
olevegic <oleg.selanta@gmail.com>
2025-10-17 00:58:41 +0300

    pre-final

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,19 +1,14 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-// Инстанс для первого сервиса
 export const apiClient = axios.create({
-  // baseURL: "/", // Указываем на корень. Прокси сам добавит /api.
   baseURL: "https://localhost:37449",
 });
 
-// Инстанс для второго сервиса
 export const service2Client = axios.create({
-  // baseURL: "/s2/api/heroes",
-  baseURL: "https://localhost:37672",
+  baseURL: "https://localhost:37672/api/heroes",
 });
 
-// Централизованный обработчик ошибок (без изменений)
 const errorHandler = (error: any) => {
   if (axios.isAxiosError(error)) {
     const errorMessage = error.response?.data?.message || error.message;
